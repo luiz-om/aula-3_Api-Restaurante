c800a61d31123574940a59e5c1c64e8f78e76e61
Luiz-om <luizfernando_martins@hotmail.com>
2023-03-14 21:05:53 -0400

    Rotas

---
+++ src/db/MesaDB.js
@@ -0,0 +1,6 @@
+const DB_MESAS = [
+    {numero:'01'}
+]
+
+
+module.exports= DB_MESAS
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const restaurente = express()
+
+restaurente.use(express.json())
+
+
+
+
+
+//importacao de rotas
+const MesaRouter = require('./routes/MesaRoute.js')
+
+
+restaurente.use(MesaRouter)
+
+
+
+
+
+//inicializacao da API
+restaurente.listen(4000,()=>{
+    console.log('Api rodando');
+})
